93e513a4ca96040958458d64e8c9ceb81f5f34a1
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2019-08-15 20:24:28 +0900

    done
    done

--- 40_Membership_Operator/main.py
+++ 40_Membership_Operator/main.py
@@ -7,6 +7,7 @@
 x = ["apple", "banana"]
 
 print("banana" in x)
+print("kagaya" in x)
 
 # returns True because a sequence with the value "banana" is in the list
 
---
+++ 41_Membership_Operator/main.py
@@ -0,0 +1,12 @@
+
+
+#Operator	Description	                                                                      Example
+#not in	Returns True if a sequence with the specified value is not present in the object	x not in y
+
+
+x = ["apple", "banana"]
+
+print("pineapple" not in x)
+print("banana" not in x)
+
+# returns True because a sequence with the value "pineapple" is not in the list
